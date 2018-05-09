453547e9773c70522fe10841563b45d560183d0a
MCCA01 <teckonn@crenergy.com.my>
2018-05-10 00:39:44 +0800

    Added stories in Home Page

--- Components/AppTabNavigator/HomeTab.js
+++ Components/AppTabNavigator/HomeTab.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    ScrollView
 } from 'react-native';
 
-import { Container, Content, Icon } from 'native-base';
+import { Container, Content, Icon, Thumbnail } from 'native-base';
 import CardComponent from '../CardComponent';
 
 class HomeTab extends Component {
@@ -20,6 +21,57 @@ class HomeTab extends Component {
         return (
             <Container style={styles.container}>
                 <Content>
+                    <View style={{ height: 100 }}>
+                        <View style={{
+                            flex: 1,
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            paddingHorizontal: 7
+                        }}>
+                            <Text style={{ fontWeight: "bold" }}>Stories</Text>
+                                <View
+                                    style={{flexDirection: "row", "alignItems": "center"}}>
+                                    <Icon name="md-play" style={{ fontSize: 14 }} />
+                                    <Text style={{ fontWeight: "bold" }}>Watch All</Text>
+                                </View>
+                        </View>
+                        <View style={{ flex: 3 }}>
+                            <ScrollView
+                                horizontal={true}
+                                showsHorizontalScrollIndicator={false}
+                                contentContainerStyle={{
+                                    alignItems: "center",
+                                    paddingStart: 5,
+                                    paddingEnd: 5
+                                }}>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/StoriesHeaderThumbnailSamplesImages/1.jpeg')}> 
+                                </Thumbnail>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/StoriesHeaderThumbnailSamplesImages/2.jpeg')}> 
+                                </Thumbnail>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/cat.jpg')}> 
+                                </Thumbnail>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/StoriesHeaderThumbnailSamplesImages/3.jpg')}> 
+                                </Thumbnail>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/StoriesHeaderThumbnailSamplesImages/4.png')}> 
+                                </Thumbnail>
+                                <Thumbnail 
+                                    style={{marginHorizontal: 5, borderColor: "pink", borderWidth: 2}} 
+                                    source={require('../../assets/StoriesHeaderThumbnailSamplesImages/5.png')}> 
+                                </Thumbnail>
+                            </ScrollView>
+                        </View>
+                    </View>
                     <CardComponent imageSource="1" likes="312"/>
                     <CardComponent imageSource="2" likes="12"/>
                     <CardComponent imageSource="3" likes="902"/>
